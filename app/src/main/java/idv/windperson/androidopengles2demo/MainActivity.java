cb104b33518ce3d047540ee561fe0066d0b5a43b
windperson <windperson@gmail.com>
2016-09-05 16:16:34 +0800

    add normal button on top of OpenGL drawing area via Activitiy's "addContentView()" API

--- app/src/main/java/idv/windperson/androidopengles2demo/MainActivity.java
+++ app/src/main/java/idv/windperson/androidopengles2demo/MainActivity.java
@@ -1,8 +1,14 @@
 package idv.windperson.androidopengles2demo;
 
 import android.opengl.GLSurfaceView;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.view.ViewGroup.LayoutParams;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +21,34 @@ public class MainActivity extends AppCompatActivity {
         _surfaceView.setEGLContextClientVersion(2);
         _surfaceView.setRenderer(new GLES2Renderer());
         setContentView(_surfaceView);
+
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.VERTICAL);
+        layout.setPadding(0, 200, 0, 0);
+
+        Button buttonUp = new Button(this);
+        buttonUp.setText("UP");
+        buttonUp.setWidth(110);
+        buttonUp.setHeight(85);
+        LinearLayout.LayoutParams layoutParamsUpBtn =
+                new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        layoutParamsUpBtn.setMargins(0, 0, 0, 20);
+        layout.addView(buttonUp, layoutParamsUpBtn);
+
+        Button buttonDown = new Button(this);
+        buttonDown.setText("DOWN");
+        buttonDown.setWidth(110);
+        buttonDown.setHeight(85);
+        LinearLayout.LayoutParams layoutParamsDownBtn =
+                new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        layoutParamsDownBtn.setMargins(0, 20, 0, 0);
+        layout.addView(buttonDown, layoutParamsDownBtn);
+
+        layout.setGravity(Gravity.CENTER | Gravity.END);
+
+        addContentView(layout, new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+
     }
 }
