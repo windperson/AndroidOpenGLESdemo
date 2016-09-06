1a1b8f2b10f8dacb9dbdb98bbed8a5d392cb606d
windperson <windperson@gmail.com>
2016-09-06 15:34:45 +0800

    runnable

--- app/src/main/java/idv/windperson/androidopengles2demo/GLES2Renderer.java
+++ app/src/main/java/idv/windperson/androidopengles2demo/GLES2Renderer.java
@@ -6,23 +6,142 @@ import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 import android.opengl.GLES20;
+import android.opengl.Matrix;
+//import android.util.Log;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+import java.nio.ShortBuffer;
 
 /**
  * Created by windperson on 9/5/2016.
  */
 public class GLES2Renderer implements GLSurfaceView.Renderer {
+    private FloatBuffer _VFBarrow;
+    private ShortBuffer _ISBarrow;
+    private int[] _arrowBuffers = new int[2];
+    private int _arrowProgram;
+    private int _arrowAPositionLocation;
+    private int _arrowUMVPLocation;
+    private float[] _RMatrix = new float[16];
+    private static volatile float _zAngle;
+
+
     @Override
     public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
-        GLES20.glClearColor(0.0f,1.0f, 0.0f, 1);
+        GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1);
     }
 
     @Override
     public void onSurfaceChanged(GL10 gl10, int width, int height) {
         GLES20.glViewport(0, 0, width, height);
+        initArrow();
+
+        int arrowVertexShader = loadShader(GLES20.GL_VERTEX_SHADER, _arrowVertexShaderCode);
+        int arrowFragmentShader = loadShader(GLES20.GL_FRAGMENT_SHADER, _arrowFragmentShaderCode);
+        _arrowProgram = GLES20.glCreateProgram();
+        GLES20.glAttachShader(_arrowProgram, arrowVertexShader);
+        GLES20.glAttachShader(_arrowProgram, arrowFragmentShader);
+        GLES20.glLinkProgram(_arrowProgram);
+
+        _arrowAPositionLocation = GLES20.glGetAttribLocation(_arrowProgram, "aPosition");
+        _arrowUMVPLocation = GLES20.glGetUniformLocation(_arrowProgram, "uMVP");
+        Matrix.setIdentityM(_RMatrix, 0);
     }
+
+    private void initArrow() {
+        float[] arrowVFA = {
+                0.000467f,0.205818f,-0.000364f,
+                0.026045f,-0.113806f,-0.020301f,
+                -0.026045f,-0.113806f,-0.020301f,
+                -0.000467f,0.205818f,-0.000364f,
+                0.000467f,0.205818f,0.000364f,
+                0.026045f,-0.113806f,0.020301f,
+                -0.026045f,-0.113806f,0.020301f,
+                -0.000467f,0.205818f,0.000364f,
+        };
+
+        short[] arrowISA = {
+                0,1,3,
+                4,7,6,
+                0,4,1,
+                1,5,2,
+                2,6,3,
+                4,0,7,
+                1,2,3,
+                5,4,6,
+                4,5,1,
+                5,6,2,
+                6,7,3,
+                0,3,7,
+        };
+
+        ByteBuffer arrowVBB = ByteBuffer.allocateDirect(arrowVFA.length * 4);
+        arrowVBB.order(ByteOrder.nativeOrder());
+        _VFBarrow = arrowVBB.asFloatBuffer();
+        _VFBarrow.put(arrowVFA);
+        _VFBarrow.position(0);
+
+        ByteBuffer arrowIBB = ByteBuffer.allocateDirect(arrowISA.length * 2);
+        arrowIBB.order(ByteOrder.nativeOrder());
+        _ISBarrow = arrowIBB.asShortBuffer();
+        _ISBarrow.put(arrowISA);
+        _ISBarrow.position(0);
+
+        GLES20.glGenBuffers(2, _arrowBuffers, 0);
+        GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, _arrowBuffers[0]);
+        GLES20.glBufferData(GLES20.GL_ARRAY_BUFFER, arrowVFA.length * 4, _VFBarrow, GLES20.GL_STATIC_DRAW);
+        GLES20.glBindBuffer(GLES20.GL_ELEMENT_ARRAY_BUFFER, _arrowBuffers[1]);
+        GLES20.glBufferData(GLES20.GL_ELEMENT_ARRAY_BUFFER, arrowISA.length * 2, _ISBarrow, GLES20.GL_STATIC_DRAW);
+    }
+
+    private int loadShader(int type, String source)  {
+        int shader = GLES20.glCreateShader(type);
+        GLES20.glShaderSource(shader, source);
+        GLES20.glCompileShader(shader);
+        return shader;
+    }
+
+    private final String _arrowVertexShaderCode =
+                        "attribute vec3 aPosition;											\n"
+                    +	"uniform mat4 uMVP;													\n"
+                    +	"void main() {														\n"
+                    +	" vec4 vertex = vec4(aPosition[0],aPosition[1],aPosition[2],1.0);	\n"
+                    +	" gl_Position = uMVP * vertex;										\n"
+                    +	"}																	\n";
+    private final String _arrowFragmentShaderCode =
+                        "#ifdef GL_FRAGMENT_PRECISION_HIGH		\n"
+                    +	"precision highp float;					\n"
+                    +	"#else									\n"
+                    +	"precision mediump float;				\n"
+                    +	"#endif									\n"
+                    +	"void main() {							\n"
+                    +	" gl_FragColor = vec4(1.0,1.0,0.0,1);	\n"
+                    +	"}										\n";
 
     @Override
     public void onDrawFrame(GL10 gl10) {
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
+        Matrix.setIdentityM(_RMatrix, 0);
+        Matrix.rotateM(_RMatrix, 0, _zAngle, 0, 0, 1);
+
+        GLES20.glUseProgram(_arrowProgram);
+        GLES20.glUniformMatrix4fv(_arrowUMVPLocation, 1, false, _RMatrix, 0);
+        GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, _arrowBuffers[0]);
+        GLES20.glVertexAttribPointer(_arrowAPositionLocation, 3, GLES20.GL_FLOAT, false, 12, 0);
+        GLES20.glEnableVertexAttribArray(_arrowAPositionLocation);
+        GLES20.glBindBuffer(GLES20.GL_ELEMENT_ARRAY_BUFFER, _arrowBuffers[1]);
+        GLES20.glDrawElements(GLES20.GL_TRIANGLES, 36, GLES20.GL_UNSIGNED_SHORT, 0);
+        System.gc();
     }
+
+    public static void setZAngle(float angle) {
+        GLES2Renderer._zAngle = angle;
+    }
+
+    public static float getZAngle() {
+        return GLES2Renderer._zAngle;
+    }
+
 }
--- app/src/main/java/idv/windperson/androidopengles2demo/MainActivity.java
+++ app/src/main/java/idv/windperson/androidopengles2demo/MainActivity.java
@@ -3,10 +3,24 @@ package idv.windperson.androidopengles2demo;
 import android.opengl.GLSurfaceView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+//import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.RelativeLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
 
     private GLSurfaceView _surfaceView;
+    private float _dxFiltered = 0.0f;
+    private float _zAngle = 0.0f;
+    private float _zAngleFiltered = 0.0f;
+
+    private int _width;
+    private final float _filterSensitivity = 0.1f;
+    private final float _TOUCH_SENSITIVITY = 0.6f;
+    private float _ANGLE_SPAN = 90.0f;
+    private float _touchedX;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +29,68 @@ public class MainActivity extends AppCompatActivity {
         _surfaceView.setEGLContextClientVersion(2);
         _surfaceView.setRenderer(new GLES2Renderer());
         setContentView(_surfaceView);
+        RelativeLayout rl = new RelativeLayout(this);
+        rl.setOnTouchListener(this);
+        RelativeLayout.LayoutParams rllp = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.MATCH_PARENT,
+                RelativeLayout.LayoutParams.MATCH_PARENT);
+        addContentView(rl, rllp);
+        getDeviceWidth();
+    }
+
+    @Override
+    protected void onPause() {
+        _surfaceView.onPause();
+        super.onPause();
+        if (isFinishing()) {
+            // save high scores etc
+            GLES2Renderer.setZAngle(0);
+            _dxFiltered = 0.0f;
+            _zAngle = 0.0f;
+            _zAngleFiltered = 0.0f;
+            this.finish();
+        }
+    }
+
+    public void getDeviceWidth() {
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+        if (width > height) {
+            _width = width;
+        } else {
+            _width = height;
+        }
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            _touchedX = event.getX();
+        } else if (event.getAction() == MotionEvent.ACTION_MOVE) {
+            float touchedX = event.getX();
+            float dx = Math.abs(_touchedX - touchedX);
+            _dxFiltered = _dxFiltered * (1.0f - _filterSensitivity) + dx
+                    * _filterSensitivity;
+
+            if (touchedX < _touchedX) {
+                _zAngle = (2 * _dxFiltered / _width) * _TOUCH_SENSITIVITY
+                        * _ANGLE_SPAN;
+                _zAngleFiltered = _zAngleFiltered * (1.0f - _filterSensitivity)
+                        + _zAngle * _filterSensitivity;
+                GLES2Renderer.setZAngle(GLES2Renderer.getZAngle()
+                        + _zAngleFiltered);
+            } else {
+                _zAngle = (2 * _dxFiltered / _width) * _TOUCH_SENSITIVITY
+                        * _ANGLE_SPAN;
+                _zAngleFiltered = _zAngleFiltered * (1.0f - _filterSensitivity)
+                        + _zAngle * _filterSensitivity;
+                GLES2Renderer.setZAngle(GLES2Renderer.getZAngle()
+                        - _zAngleFiltered);
+            }
+        }
+        //Log.d("onTouch","_zAngle="+_zAngle);
+        return true;
     }
 }
